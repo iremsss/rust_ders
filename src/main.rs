52336cc59c740268cf38e40efe5c3f1bd3b0ea86
iremsss <iremsaygin1997@gmail.com>
2023-06-21 17:46:53 +0300

    son hali

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,35 @@
-use core::num;
-
 //Create a simple calculator using enums and pattern matching
 /*
 enum olusturmak lazım
 0 a bölünebilme icin bitane kontrol lazım
 4 tane fonksiyon yapmak lazım- acaba fonksiyonlar enumun mu olcak yoksa ayrı fonksiynlar mı
  */
- enum Operation{
-    Add,
-    Sub,
-    Mul,
-    Div,
+enum Operation{
+    _Add,
+    _Sub,
+    _Mul,
+    _Div,
 }
-fn calculate(number1 : f64, number2 : f64, op: Operation) -> f64{
+
+fn calculate(number1 : f64, number2 : f64, op: Operation) -> Result<f64, &'static str>{
     match op{
-        Operation::Add => {
-            return number1 + number2;
-        }
-        Operation::Sub => {
-            return number1 - number2;
-        }
-        Operation::Mul => {
-            return number1*number2;
-        }
-        Operation::Div => {
-            return  number1/number2;
+        Operation::_Add => Ok(number1 + number2),
+        Operation::_Sub => Ok(number1 - number2),
+        Operation::_Mul => Ok(number1 * number2),
+        Operation::_Div => {
+            if number2 == 0.0 {
+                Err("Cannot divide by zero")
+            } else {
+                Ok(number1 / number2)
+            }
         }
     }
 }
 
 fn main() {
-    let op = Operation::Div;
-    let result = calculate(1.0, 1.0,op);
-    println!("result is : {} " , result);
+    let op = Operation::_Div;
+    match calculate(1.0, 6.0, op) {
+        Ok(result) => println!("Result is : {} ", result),
+        Err(err) => println!("Error: {}", err),
+    }
 }
-
-
